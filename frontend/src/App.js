598a1fbf7e8368025e6981a2301c7ab0c88d72d2
Jan Slivka <jan.slivka.workspace@seznam.cz>
2022-12-07 20:44:36 +0100

    Fixed frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+//import Team from "./scenes/team";
+//import RandomNazev1 from "./scenes/randomnazev1";
+//import RandomNazev2 from "./scenes/randomnazev2";
+//import Bar from "./scenes/bar";
+//import Profile from "./scenes/profile";
+//import Line from "./scenes/line";
+//import Pie from "./scenes/pie";
+//import FAQ from "./scenes/faq";
+//import Calendar from "./scenes/calendar";
+
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/*<Route path="/team" element={<Team />} />*/}
+              {/*<Route path="/randomnazev1" element={<RandomNazev1 />} />*/}
+              {/*<Route path="/randomnazev2" element={<RandomNazev2 />} />*/}
+              {/*<Route path="/profile" element={<Profile />} />*/}
+              {/*<Route path="/bar" element={<Bar />} />*/}
+              {/*<Route path="/pie" element={<Pie />} />*/}
+              {/*<Route path="/line" element={<Line />} />*/}
+              {/*<Route path="/faq" element={<FAQ />} />*/}
+              {/*<Route path="/calendar" element={<Calendar />} />*/}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
---
+++ frontend/src/scenes/dashboard/index.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header
+          title="DASHBOARD"
+          subtitle="Zdarec, jeste nevim jestli mam psat v cz nebo eng :-)"
+        ></Header>
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
