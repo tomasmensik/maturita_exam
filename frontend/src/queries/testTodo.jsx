1948526ee65df6e300acb4247a8c743ac3ece621
Jan Slivka <jan.slivka.workspace@seznam.cz>
2022-12-28 20:51:31 +0100

    Final connection

--- frontend/src/queries/testTodo.jsx
+++ frontend/src/queries/testTodo.jsx
@@ -24,13 +24,22 @@ function UserList() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  return (
-    <ul>
-      {data.profiles.map(profiles => (
-        <li key={profiles.id}>{profiles.idProfile}</li>
-      ))}
-    </ul>
-  );
-}
+ 
+    return (
+      <ul>
+        {data.profiles.map(profile => (
+          <li key={profile.id}>
+            {profile.idProfile}: {profile.profileGit} ({profile.profileTime})
+            {profile.profileProject.map(project => (
+              <div key={project.proName}>
+                <h3>{project.proName}</h3>
+                <p>{project.proDescription}</p>
+              </div>
+            ))}
+          </li>
+        ))}
+      </ul>
+    );
+    }
 
 export default UserList;
