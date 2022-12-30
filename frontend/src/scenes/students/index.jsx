54f2086a5dfa300aff83df0de7262b08fc03fbc0
Jan Slivka <jan.slivka.workspace@seznam.cz>
2022-12-30 03:41:18 +0100

    Update of api

--- frontend/src/scenes/students/index.jsx
+++ frontend/src/scenes/students/index.jsx
@@ -13,11 +13,79 @@ import { mockDataTeam } from "../../data/mockData";
 import LaptopChromebookIcon from "@mui/icons-material/LaptopChromebook";
 import NetworkCheckIcon from "@mui/icons-material/NetworkCheck";
 import Header from "../../components/Header";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
 
 //Tato arrow Students() function nam umozni vygenerovat "Students" studentu. Diky teto tabulce muze admin managovat se studenty.
 //Nejprve si vytvorime nase sloupce (neboli hlavicku) a napise jednotlive nazvy.
 //Pomoci rendercell() funkce vyrenderujeme jednotlive studenty a nastavime barvu podle toho, jestli jsou programatori nebo sitari.
 
+const GET_USERS = gql`
+  query{
+    profiles{
+      id
+      profileGit
+      proClass
+      idProfile
+      user{
+        id
+        email
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+const Students = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const { data, loading, error } = useQuery(GET_USERS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <Table>
+    <TableHead>
+      <TableRow>
+        <TableCell>ID</TableCell>
+        <TableCell>Git Profile</TableCell>
+        <TableCell>Class</TableCell>
+        <TableCell>Last Name</TableCell>
+        <TableCell>Email</TableCell>
+        <TableCell>Student</TableCell>
+
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {data.profiles.map(profile => (
+        <TableRow key={profile.id}>
+          <TableCell>{profile.id}</TableCell>
+          <TableCell>{profile.user.firstName} {profile.user.lastName}</TableCell>
+          <TableCell>{profile.idProfile}</TableCell>
+          <TableCell>{profile.profileGit}</TableCell>
+          <TableCell>{profile.user.email}</TableCell>
+          <TableCell>{profile.proClass}</TableCell>
+
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+  
+    );
+
+}
+
+export default Students;
+
+
+
+/*
 const Students = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
@@ -119,3 +187,5 @@ const Students = () => {
 };
 
 export default Students;
+
+*/
